f008906343846265b1b2162cbaccb0bb77d633fd
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-12-23 19:57:46 +0530

    new-4

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// src/setupTests.js
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    Link: ({ children }) => <a>{children}</a>, // Mock the Link component
+  }));
+  
