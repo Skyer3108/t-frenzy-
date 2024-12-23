125c8aca9f65f719f9a92e67e12dc63a8a6ad979
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-12-23 16:51:15 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express=require('express')
-
+const helmet=require('')
 const app=express()
 const cors=require('cors')
 const dotenv=require('dotenv')
@@ -13,8 +13,13 @@ const userRoute=require('./Router/userRoute')
 const PORT=4000;
 
 app.use(express.json())
-app.use(cors())
+ app.use(cors())
 
+ app.use(express.static("./frontend/build"))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'frontend','build',"index.html"))
+})
 app.use('/api/user',userRoute)
 
 
